d31b571390377a33c5eb17cae2c3fd388b9da21b
shiinay <shiinay@C010423280.zipangu.cac.co.jp>
2026-04-30 12:35:11 +0900

    q02_advanced.question01を完了
    理解できた点
    ・集約の存在
    
    不明な点、調査中
    ・集約の扱い方

--- java_comprehension_exercises_volume2_NO_NAME/src/q02_advanced/question01/SystemMain01.java
+++ java_comprehension_exercises_volume2_NO_NAME/src/q02_advanced/question01/SystemMain01.java
@@ -5,7 +5,12 @@ public class SystemMain01 {
 	public static void main(String[] args) {
 
 		System.out.println("===会員情報を表示します===");
-		//TODO ここから実装する
+		//以下
+		Member member1 = new Member(1, "Passw0rd", "Miura Manabu", 28, 2);
+		Member member2 = new Member(2, "aaaAAA", "Sato Kensuke", 43, 1);
+
+		member1.showMember();
+		member2.showMember();
 
 	}
 
